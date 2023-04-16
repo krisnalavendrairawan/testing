5bc97a5c365243fb675ba99fb857ea22e9055056
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-04-16 23:35:32 +0700

    yahaha

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-
-console.log('hello world')
-console.log('dirtod')
-console.log("daniel")
+console.log("hello world");
+console.log("dirtod");
+console.log("daniel");
 console.log("kristol");
 console.log("hello world");
 console.log("dirtod");
 console.log("daniel");
 console.log("dirham");
+console.log("yahahaha");
